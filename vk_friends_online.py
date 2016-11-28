f3c94c43706e9f636e218332070f6f56c7e7ce13
Alexander <Alexander>
2016-11-28 14:00:01 +0300

    Джобс дан

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,18 @@
 import vk
+from getpass import getpass
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 5750748
 
 
 def get_user_login():
-    pass
+    login = input('Login: ')
+    return login
 
 
 def get_user_password():
-    pass
+    password = getpass()
+    return password
 
 
 def get_online_friends(login, password):
@@ -17,16 +20,21 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope=2,
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    online_friends_id = api.friends.getOnline()
+    online_friends_names_json = api.users.get(user_ids=online_friends_id)
+    return online_friends_names_json
 
 
-def output_friends_to_console(friends_online):
-    pass
+def output_friends_to_console(online_friends_names_json):
+    for online_friend_information in online_friends_names_json:
+        print('{0} {1}'.format(online_friend_information['last_name'], online_friend_information['first_name']))
+
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    online_friends_names_json = get_online_friends(login, password)
+    output_friends_to_console(online_friends_names_json)
